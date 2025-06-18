cbf8a76dbd93560ce55c65513c3572c904d6d824
sonika-tang <sonikatang630@gmail.com>
2025-06-18 23:03:54 +0700

    week6 assignment

---
+++ back/src/utils/database.js
@@ -0,0 +1,10 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+ 
+dotenv.config();
+ 
+// TODO
+// Create the pool to connect to the database
+// Use the database settings from the .env file
+
+export { pool };
