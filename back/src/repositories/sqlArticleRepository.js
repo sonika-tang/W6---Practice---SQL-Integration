b491086bcabebd41e0a0774ca457cfd188adf4c4
sonika-tang <sonikatang630@gmail.com>
2025-06-18 23:13:46 +0700

    update connection to database

--- back/src/repositories/sqlArticleRepository.js
+++ back/src/repositories/sqlArticleRepository.js
@@ -7,24 +7,34 @@
 // Get all articles
 export async function getArticles() {
     // TODO
+    const [rows] = await pool.query("SELECT * FROM articles");
+    return rows;
 }
 
 // Get one article by ID
 export async function getArticleById(id) {
     // TODO
+    const [rows] = await pool.query("SELECT * FROM articles WHERE id = ?", [id]);
+    return rows[0];
 }
 
 // Create a new article
 export async function createArticle(article) {
     // TODO
+    const { title, content, journalist, category } = article;
+    const [result] = await pool.query("INSERT INTO articles (title, content, journalist, category) VALUES (?, ?, ?, ?)",[title, content, journalist, category]);
+    return result.insertId;
 }
 
 // Update an article by ID
 export async function updateArticle(id, updatedData) {
     // TODO
+    const { title, content, journalist, category } = updatedData;
+    await pool.query("UPDATE articles SET title = ?, content = ?, journalist = ?, category = ? WHERE id = ?",[title, content, journalist, category, id]);
 }
 
 // Delete an article by ID
 export async function deleteArticle(id) {
     // TODO
+    await pool.query("DELETE FROM articles WHERE id = ?", [id]);
 }
--- back/src/utils/database.js
+++ back/src/utils/database.js
@@ -7,4 +7,15 @@ dotenv.config();
 // Create the pool to connect to the database
 // Use the database settings from the .env file
 
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
 export { pool };
